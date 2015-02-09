96e7ac4cfa0fbfad9c03d1a131585fc4fc502abe
herch <issam.rachdi@gmail.com>
2015-02-09 15:54:17 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/skeleton.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <title></title>
+    <style>
+      [draggable] {
+        user-select: none;
+      }
+      .item {
+        height: 30px;
+        line-height: 30px;
+        width: 150px;
+        border: 1px solid #ccc;
+        margin-bottom: 5px;
+        border-radius: 5px;
+        text-align: center;
+        cursor: move;
+      }
+      .item.over {
+        border: 1px dashed #000;
+      }
+      #form_task label {
+        display: inline-block;
+      }
+      #form_task button {
+        display: inline;
+        vertical-align: middle;
+      }
+      #form_task input {
+        width: 400px;
+      }
+      #form_task button, #form_task select, #form_task input, #form_task label {
+        margin-bottom: 0;
+        margin: 4px;
+      }
+
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div id="form_task" class="div_row">
+            <label>Title</label><input type="text" id="title">
+            <label>Priority</label>
+            <select id="priority" >
+              <option value="1">High</option>
+              <option value="2">Normal</option>
+              <option value="3">Low</option>
+            </select>
+            <button class="button" id="btn_new_task"><i class="fa fa-plus"></i> Create task</button>
+        </div>
+      </div>
+      
+        <div id="tasks">
+          <!--      <div class="item" draggable="true">Item1</div>
+                <div class="item" draggable="true">Item2</div>
+                <div class="item" draggable="true">Item3</div>-->
+        </div> 
+    </div>
+    <script src="js/dnd.js"></script>
+    <script>
+      function newTask() {
+        console.log('new task');
+      }
+      
+      window.onload = function() {
+        var btnNewTask = document.getElementById('btn_new_task');
+        btnNewTask.addEventListener('click', newTask, false);
+      }
+    </script>
+  </body>
+</html>
---
+++ js/dnd.js
@@ -0,0 +1,56 @@
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  e.preventDefault(); // Necessary. Allows us to drop.
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+  return false;
+}
+
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+function handleDrop(e) {
+  // this / e.target is current target element.
+  e.stopPropagation(); // stops the browser from redirecting.
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+  this.style.opacity = '1';
+  [].forEach.call(items, function (item) {
+    item.classList.remove('over');
+  });
+}
+
+var items = document.querySelectorAll('#items .item');
+[].forEach.call(items, function (item) {
+  item.addEventListener('dragstart', handleDragStart, false);
+  item.addEventListener('dragenter', handleDragEnter, false);
+  item.addEventListener('dragover', handleDragOver, false);
+  item.addEventListener('dragleave', handleDragLeave, false);
+  item.addEventListener('drop', handleDrop, false);
+  item.addEventListener('dragend', handleDragEnd, false);
+})
